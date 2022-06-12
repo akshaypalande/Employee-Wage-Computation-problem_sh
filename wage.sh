5ad93f2e20ebe715e61091c6a7bbf4743d88eae7
akshaypalande <59253318+akshaypalande@users.noreply.github.com>
2022-06-12 18:18:10 +0530

    [Akshay] add : monthly salary for employee

--- wage.sh
+++ wage.sh
@@ -1,50 +1,30 @@
-#!/bin/bash
+#!/bin/bash 
 
-echo "Welcome to Employee Wage Computation Program"
-
-#Check Employee is Present or Absent
-
-wage=$(( RANDOM%2 ))
-if (( $wage==1 ))
-then
-	echo "Employee is Present"
-else
-	echo "Employee is Absent"
-fi
-
-# Calculate Daily Employee Wage
-
-
-Dailywage=$(( 8*20 ))
-echo "Daily wage of a Employee is : "$Dailywage
-
-# calculate part time employee wage
-
-PartTimeWage=$((4*20))
-
-echo "Parttime wage of Employee is : "$PartTimeWage
-
-
-# do using switch case
+echo " Welcome To Employee Wage Computation Problem "
 
 PartTime=1
 FullTime=2
+totalwage=0;
 empRatePerHrs=20;
+NumberOfWorkingDays=20;
 
-randomCheck=$(($RANDOM%2));
+for(( Day=1; Day<=$NumberOfWorkingDays; Day++ ))
+do
+	randomCheck=$(($RANDOM%2));
+	case $randomCheck in
+		$FullTime)
+			empHrs=8
+			;;
+		$PartTime)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
 
-case $randomCheck in
-	$FullTime)
-		echo "Employee is Full Time"
-		empHrs=8
-		;;
-	$PartTime)
-		echo "Employee is Part Time"
-		empHrs=4
-		;;
-	*)
-	empHrs=0
-		;;
-esac
-salary=$(($empHrs*$empRatePerHrs));
-echo "Employee salary " $salary;
+wage=$(($empHrs*$empRatePerHrs));
+totalwage=$(($totalwage+$wage))
+
+done
+echo "Employee Wage " $totalwage;
