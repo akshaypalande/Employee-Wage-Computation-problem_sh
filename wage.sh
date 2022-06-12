17d4d6eeeea6a33b0d0a5c97fc2aa11a30a3fcb9
akshaypalande <59253318+akshaypalande@users.noreply.github.com>
2022-06-12 17:16:32 +0530

    [Akshay] add: Calculate dail wage

--- wage.sh
+++ wage.sh
@@ -4,11 +4,15 @@ echo "Welcome to Employee Wage Computation Program"
 
 #Check Employee is Present or Absent
 
-A=$(( RANDOM % 2 ))
-if (( $A == 1 ))
+wage=$(( RANDOM%2 ))
+if (( $wage==1 ))
 then
 	echo "Employee is Present"
 else
 	echo "Employee is Absent"
 fi
 
+# Calculate Daily Employee Wage
+
+Dailywage=$(( 8*20 ))
+echo "Daily wage of a Employee is : "$Dailywage
