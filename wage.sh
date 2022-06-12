783c4c45914511efc7850506bb2a0b6e687b74cb
akshaypalande <59253318+akshaypalande@users.noreply.github.com>
2022-06-12 18:08:13 +0530

    [Akshay] add: solved using switch casch caseh case

--- wage.sh
+++ wage.sh
@@ -14,11 +14,37 @@ fi
 
 # Calculate Daily Employee Wage
 
+
 Dailywage=$(( 8*20 ))
 echo "Daily wage of a Employee is : "$Dailywage
 
-# calculate part employee wage
+# calculate part time employee wage
 
 PartTimeWage=$((4*20))
 
 echo "Parttime wage of Employee is : "$PartTimeWage
+
+
+# do using switch case
+
+PartTime=1
+FullTime=2
+empRatePerHrs=20;
+
+randomCheck=$(($RANDOM%2));
+
+case $randomCheck in
+	$FullTime)
+		echo "Employee is Full Time"
+		empHrs=8
+		;;
+	$PartTime)
+		echo "Employee is Part Time"
+		empHrs=4
+		;;
+	*)
+	empHrs=0
+		;;
+esac
+salary=$(($empHrs*$empRatePerHrs));
+echo "Employee salary " $salary;
