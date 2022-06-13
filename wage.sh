b11591659522a2ab0667ab2310ab0aa736ddd24a
akshaypalande <59253318+akshaypalande@users.noreply.github.com>
2022-06-13 07:44:07 +0530

    [Akshay] add : UC_9

--- wage.sh
+++ wage.sh
@@ -1,59 +1,27 @@
-#!/bin/bash -x
-
-echo "Welcome To Employee Wage Compution Problem"
-
-#CONSTANT
-WAGE_PER_HOUR=20
-IS_FULL_TIME=2
-IS_PART_TIME=1
-EMPLOYEE_HOUR_FULLTIME=8
-EMPLOYEE_HOUR_PARTTIME=4
-
-# USER INPUTE
-read -p "To number of working days :" NUMBER_OF_WORKING_DAYS
-read -p "To number of working hour :" NUMBER_OF_WORKING_HOURS
-
-#VARIABLE
-totalWage=0
-totalEmployeeHours=0
-totalWorkingDays=0
-
-#TO FUNCTION TO GET WORK HOURS
-function getWorkHours()
-{
-	randomShiftCheck=$((RANDOM%3))
-	case $randomShiftCheck in
-
-		$IS_FULL_TIME )
-			employeeHour=$((EMPLOYEE_HOUR_FULLTIME))
-			;;
-		$IS_PART_TIME )
-			employeeHour=$((EMPLOYEE_HOUR_PARTTIME))
-			;;
-		* )
-			employeeHour=0
-			;;
-   esac
-	echo $employeeHour
+#!/bin/bash
+echo " Welcome to Employee Wage Problem "
+HalfTimeHours=4
+WagePerHour=20
+FullTimeHours=8
+WorkingDays=20
+MonthlyWage=0
+hours=0
+days=1
+WorkHours() {
+	local RandomCheck=$1
+	case $RandomCheck in 
+		"0") echo 0 ;;
+		"1") echo $HalfTimeHours ;;
+		"2") echo $FullTimeHours ;;
+	esac
 }
-
-#FUNCTION TO CALCULATE WAGE
-function calculateWage()
-{
-	employeeHour=$1
-	wage=$(($employeeHour*$WAGE_PER_HOUR))
-	echo $wage
-}
-
-#GET WORK HOUR FROM FUNCTION AND CALCULATE DAILY WAGE TILL CONDITION SATISFIED
-while [[ $totalEmployeeHours -lt $NUMBER_OF_WORKING_HOURS && $totalWorkingDays -lt $NUMBER_OF_WORKING_DAYS ]]
+while [[ $days -le 20 && $hours -le 100 ]]
 do
-	((totalWorkingDays++))
-	dailyWage[totalWorkingDays]=$(calculateWage $(getWorkHours))
-	totalEmployeeHours=$(($totalEmployeeHours + $(getWorkHours)))
+	RandomCheck=`echo $((RANDOM%3))`
+	DayHours="$( WorkHours $RandomCheck )"
+	DailyWage=$(( $DayHours * $WagePerHour ))
+	hours=$(( $hours + $DayHours ))
+	MonthlyWage=$(( $MonthlyWage + $DailyWage ))
+	echo -e "Day:$days       DailyWage:$DailyWage       TotalWage:$MonthlyWage"
+	days=$(( $days + 1 ))
 done
-
-#TO PRINT WAGES FOR A MONTH
-totalWageOfMonth=$(($(calculateWage $totalEmployeeHours)))
-echo "Daily wages: ${dailyWage[@]}"
-echo "Total Wage:" $totalWageOfMonth
