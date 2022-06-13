8dcbbf55a4989b74d1fad892d0d7401be74e8aaa
akshaypalande <59253318+akshaypalande@users.noreply.github.com>
2022-06-13 06:59:00 +0530

    [Akshay] add: function to calculate wage

--- wage.sh
+++ wage.sh
@@ -1,33 +1,62 @@
-#!/bin/bash 
+!/bin/bash -x
 
-echo " Welcome To Employee Wage Computation Problem "
+printf "Welcome To Employee Wage Computation Problem \n"
 
-PartTime=1
-FullTime=2
-totalHrs=0;
-TotalWorkingDays=0;
-HrsInMonth=100;
-empRatePerHrs=20;
-NumberOfWorkingDays=20;
+#CONSTANTS
+WORK=2
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
+HALF_DAY_HOUR=4
+IS_PRESENT=1
+IS_FULL_DAY=0
+IS_HALF_DAY=1
+MONTH=20
+TOTAL_HOURS=100
 
-while [[ $totalHrs -lt $HrsInMonth && $TotalWorkingDays -lt $NumberOfWorkingDays ]]
+#VARIABLES
+randomNumber=0
+randomNumber=$(( RANDOM % $WORK ))
+randomNumber2=$(( RANDOM % $WORK ))
+wage=0
+attendance=0
+totalWage=0
+totalWorkingHours=0
+days=0
+employeeHours=0
+hours=0
+
+#GET HOURS 8 or 4
+function getHours(){
+	 case $1 in
+		$IS_FULL_DAY)
+			echo $FULL_DAY_HOUR
+		;;
+		$IS_HALF_DAY)
+			echo $HALF_DAY_HOUR
+		;;
+		*)
+			echo 0
+		;;
+	esac
+}
+
+#WHILE DAY IS LESS THAN 20 AND HOURS IS LESS THAN 100 TILL CHECK
+while [ $days -lt $MONTH -a $totalWorkingHours -lt $TOTAL_HOURS ]
 do
-	((TotalWorkingDays++))
-	randomCheck=$(($RANDOM%3));
-		case $randomCheck in
-			$FullTime)
-				empHrs=8
-				;;
-			$PartTime)
-				empHrs=4
-				;;
-			*)
-				empHrs=0
-				;;
-		esac
-		totalHrs=$(($totalHrs+$empHrs));
+	days=$(( $days + 1 ))
+	case $randomNumber in
+		$IS_PRESENT)
+			attendance="Present"
+			hours="$( getHours $(( $randomNumber2 )) )"
+			wage=$(( $WAGE_PER_HOUR * $hours ))
+			employeeHours=$hours
+		;;
+		0)
+			attendance="Absent"
+			employeeHours=0
+		;;
+	esac
+   	totalWage=$(( $totalWage + $wage ))
+		totalWorkingHours=$(( $totalWorkingHours + $employeeHours ))
 done
-totalwage=$(($totalHrs*$empRatePerHrs));
-echo "Employee Work Hour "$totalHrs
-echo "Employee Salary "$totalwage
-
+	printf " Employee monthly wage is : $totalWage \n"	
